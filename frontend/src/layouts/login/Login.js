9cc322d08f2689fef885922693f0202c129cfd08
kotohata <t.kotohata@gmail.com>
2018-07-21 18:15:44 +0900

    Add sign-in page

--- frontend/src/layouts/login/Login.js
+++ frontend/src/layouts/login/Login.js
@@ -1,22 +1,61 @@
 import React, { Component } from 'react'
+import { uportSetting } from '../../util/connectors';
+import { Connect, SimpleSigner } from 'uport-connect'
+const QRCode = require('qrcode.react');
+const qrcode = require('qrcode-terminal');
 
 class Login extends Component {
   constructor(props, { authData }) {
     super(props)
     authData = this.props
+
+    this.state = {
+      uportURI: '',
+    };
+
+    this.urlHandler = this.urlHandler.bind(this);
+  }
+
+  urlHandler(uri) {
+    this.setState({ uportURI: uri });
+  }
+
+  componentDidMount() {
+    const uport = new Connect('uWifi', {
+      ...uportSetting,
+      uriHandler: this.urlHandler,
+    });
+
+    uport.requestCredentials({
+      requested: ['name', 'avatar', 'phone', 'country'],
+    }).then(credentials => {
+      console.log(credentials);
+    });
   }
 
   render() {
-    return(
+    return (
       <main className="container">
         <div className="pure-g">
           <div className="pure-u-1-1">
             <h1>uWifiへようこそ</h1>
             <p>
-                uWifiはuPortを使ったwifiシェアリングサービスです。
-                <br/>
-                uPortでログインしてチケットを購入することで、uWifiのアクセスポイントに簡単につなげることができます。
+              uWifiはuPortを使ったwifiシェアリングサービスです。
+                <br />
+              uPortでログインしてチケットを購入することで、uWifiのアクセスポイントに簡単につなげることができます。
             </p>
+
+            {(() => {
+              if (this.state.uportURI.length) {
+                return <QRCode value={this.state.uportURI} />;
+              }
+            })()}
+
+            <div className='login-button'>
+              <a className="button-success pure-button button-lg" href={this.state.uportURI}>
+                uPortで認証する
+              </a>
+            </div>
           </div>
         </div>
       </main>
--- frontend/src/util/connectors.js
+++ frontend/src/util/connectors.js
@@ -1,9 +1,11 @@
 
 import { Connect, SimpleSigner } from 'uport-connect'
 
-export let uport = new Connect('uWifi', {
-  clientId: '2owPVrs4nJSZe17fBajLa68o54JDTS6v219',
-  network: 'rinkeby',
-  signer: SimpleSigner('0f1252e4406922e7da2b1434195b9bae018e5164cc6e08bbf27919a67e7ece3f'),
-})
+export const uportSetting = {
+    clientId: '2owPVrs4nJSZe17fBajLa68o54JDTS6v219',
+    network: 'rinkeby',
+    signer: SimpleSigner('0f1252e4406922e7da2b1434195b9bae018e5164cc6e08bbf27919a67e7ece3f'),
+};
+
+export let uport = new Connect('uWifi', uportSetting)
 export const web3 = uport.getWeb3()
