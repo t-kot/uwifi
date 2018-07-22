1d4524039277636e4236c031f1e346154bd8c9da
kotohata <t.kotohata@gmail.com>
2018-07-22 11:57:59 +0900

    Add login page link

--- frontend/src/layouts/home/Home.js
+++ frontend/src/layouts/home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router'
 
 class Home extends Component {
   constructor(props, { authData }) {
@@ -12,7 +13,15 @@ class Home extends Component {
       <main className="container">
         <div className="pure-g">
           <div className="pure-u-1-1">
-            Anonymous
+            <h1>uWifi とは?</h1>
+            <p>
+                uWifiはuPortを使ったwifiシェアリングサービスです。
+                <br/>
+                一度uWifiで接続するだけで、アクセスポイントが変わっても簡単に認証できます。
+
+                <br/>
+                <Link to='/login' className='pure-button'>Login</Link>
+            </p>
           </div>
         </div>
       </main>
