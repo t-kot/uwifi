e0b9359ca87d919e76c8e1538575a2f989fb0add
kotohata <t.kotohata@gmail.com>
2018-07-22 11:21:09 +0900

    Fix address

--- frontend/src/contract.js
+++ frontend/src/contract.js
@@ -1,4 +1,4 @@
 import * as UWifiContract from '../../contracts/build/contracts/UWifiCore.json'
 
-export const addressLocation = '0xe077c3D6F6b58f5341bE3056e481d37F4376a635'
+export const addressLocation = '0xd3e2bd0c99f5d65650a6057171c57b7db80dec69'
 export const abi = UWifiContract.abi
